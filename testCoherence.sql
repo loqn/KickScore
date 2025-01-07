f0da630cdaccfe444ffbe59f6a2d7ede12483ad4
Anton Beziat <anton.beziat@etu.u-bordeaux.fr>
2025-01-07 14:35:32 +0100

    tests cas scores negatif et rencontres identiques

--- testCoherence.sql
+++ testCoherence.sql
@@ -14,6 +14,8 @@ SELECT * FROM T_MATCH_MAT;
 DROP PROCEDURE IF EXISTS UserNull;
 DROP PROCEDURE IF EXISTS TeamNull;
 DROP PROCEDURE IF EXISTS MailNotUnique;
+DROP PROCEDURE IF EXISTS ScoresNegatifs;
+DROP PROCEDURE IF EXISTS RencontresIdentiques;
 DELIMITER $$
 
 
@@ -41,28 +43,34 @@ BEGIN
             LEAVE boucle_curseur;
         END IF;
 
+        /*case 
+          when usr_id IS NULL then SELECT 'ERROR: usr_id is null in user' AS Message;
+          when 
+          else 
+        end;*/
+
         IF usr_id IS NULL THEN
-            SELECT 'ERROR: usr_id is null' AS Message;
+            SELECT 'ERROR: usr_id is null in user' AS Message;
         END IF;
 
         IF usr_fname IS NULL THEN
-            SELECT 'ERROR: usr_fname is null' AS Message;
+            SELECT 'ERROR: usr_fname is null in user' AS Message;
         END IF;
 
         IF usr_name IS NULL THEN
-            SELECT 'ERROR: usr_name is null' AS Message;
+            SELECT 'ERROR: usr_name is null in user' AS Message;
         END IF;
 
         IF usr_email IS NULL THEN
-            SELECT 'ERROR: usr_email is null' AS Message;
+            SELECT 'ERROR: usr_email is null in user' AS Message;
         END IF;
 
         IF usr_isorg IS NULL THEN
-            SELECT 'ERROR: usr_isorg is null' AS Message;
+            SELECT 'ERROR: usr_isorg is null in user' AS Message;
         END IF;
 
         IF usr_password IS NULL THEN
-            SELECT 'ERROR: usr_password is null' AS Message;
+            SELECT 'ERROR: usr_password is null in user' AS Message;
         END IF;
 
     END LOOP boucle_curseur;
@@ -73,30 +81,30 @@ END$$
 
 CREATE PROCEDURE TeamNull()
 BEGIN
-    DECLARE tea_id INT;
-    DECLARE tea_name VARCHAR(255);
+    DECLARE tea_id2 INT;
+    DECLARE tea_name2 VARCHAR(255);
     DECLARE done INT DEFAULT 0;
 
-
-    DECLARE c CURSOR FOR SELECT tea_ID, tea_name FROM T_TEAM_TEA;
+    DECLARE c CURSOR FOR SELECT TEA_ID, TEA_NAME FROM T_TEAM_TEA;
 
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
     OPEN c;
 
     boucle_curseur: LOOP
-        FETCH c INTO tea_ID, tea_name;
+    
+        FETCH c INTO tea_id2, tea_name2;
 
-        IF done = 1 THEN 
+        IF done = 1 THEN
             LEAVE boucle_curseur;
         END IF;
 
-        IF tea_id IS NULL THEN
-            SELECT 'ERROR: usr_id is null' AS Message;
+        IF tea_id2 IS NULL THEN
+            SELECT 'ERROR: TEA_ID is null in team' AS Message;
         END IF;
 
-        IF tea_name IS NULL THEN
-            SELECT 'ERROR: usr_fname is null' AS Message;
+        IF tea_name2 IS NULL THEN
+            SELECT 'ERROR: TEA_NAME is null in team' AS Message;
         END IF;
 
     END LOOP boucle_curseur;
@@ -107,35 +115,133 @@ END$$
 
 CREATE PROCEDURE MailNotUnique()
 BEGIN
+    DECLARE email_id INT;
+    DECLARE email_id2 INT;
     DECLARE usr_email VARCHAR(255);
+    DECLARE usr_email2 VARCHAR(255);
     DECLARE done INT DEFAULT 0;
 
-    DECLARE c CURSOR FOR SELECT usr_email FROM T_USER_USR;
+    DECLARE c CURSOR FOR SELECT email_id, usr_email FROM T_USER_USR;
 
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
     OPEN c;
 
     boucle_curseur: LOOP
-        FETCH c INTO usr_email;
+        FETCH c INTO email_id, usr_email;
 
         IF done = 1 THEN
             LEAVE boucle_curseur;
         END IF;
 
-        SELECT CONCAT('Processing email: ', usr_email) AS Email_Info;
+            boucle_curseur2: LOOP
+                FETCH c INTO email_id2, usr_email2;
+
+                IF done = 1 THEN
+                    LEAVE boucle_curseur2;
+                END IF;
+                
+                IF usr_email = usr_email2 and email_id != email_id2 THEN
+                    SELECT 'ERROR: usr' AS Message;
+                END IF;
+
+            END LOOP boucle_curseur2;
+    
 
     END LOOP boucle_curseur;
 
-    -- Fermer le curseur
+    CLOSE c;
+END$$
+
+CREATE PROCEDURE ScoresNegatifs()
+BEGIN
+    DECLARE greenScore INT;
+    DECLARE blueScore INT;
+    DECLARE done INT DEFAULT 0;
+
+    DECLARE c CURSOR FOR SELECT MAT_GREENSCORE, MAT_BLUESCORE FROM T_MATCH_MAT;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN c;
+
+    boucle_curseur: LOOP
+    
+        FETCH c INTO greenScore, blueScore;
+
+        IF done = 1 THEN
+            LEAVE boucle_curseur;
+        END IF;
+
+        IF greenScore < 0 THEN
+            SELECT 'ERROR: green score < 0' AS Message;
+        END IF;
+
+        IF blueScore < 0 THEN
+            SELECT 'ERROR: blue score < 0' AS Message;
+        END IF;
+
+    END LOOP boucle_curseur;
+
     CLOSE c;
 END$$
 
 
+CREATE PROCEDURE RencontresIdentiques()
+BEGIN
+    DECLARE id_tea_blue INT;
+    DECLARE id_tea_green INT;
+    DECLARE id_mat1 INT;
+    DECLARE id_mat2 INT;
+    DECLARE done INT DEFAULT 0;
+
+    DECLARE c CURSOR FOR SELECT MAT_ID, TEA_ID, TEA_ID_MAT_TEAMGREEN FROM T_MATCH_MAT;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN c;
+
+    boucle_curseur: LOOP
+
+        FETCH c INTO id_mat1 ,id_tea_green, id_tea_blue;
+
+        IF done = 1 THEN
+            LEAVE boucle_curseur;
+        END IF;
+
+        boucle_curseur2: LOOP
+    
+            FETCH c INTO id_mat2 ,id_tea_green, id_tea_blue;
+
+            IF done = 1 THEN
+                LEAVE boucle_curseur2;
+            END IF;
+
+            IF id_tea_blue = id_tea_green and id_mat1 != id_mat2 THEN
+                LEAVE boucle_curseur2;
+            END IF;
+
+        END LOOP boucle_curseur2;
+        
+
+    END LOOP boucle_curseur;
+
+    CLOSE c;
+END$$
+
+
+
 DELIMITER ; 
+
+
+
+
 
 call UserNull;
 call TeamNull;
+call MailNotUnique;
+call ScoresNegatifs;
+call RencontresIdentiques;
 
 ROLLBACK;
 
