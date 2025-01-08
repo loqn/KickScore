20a21098c94ff5e8b0480f9dc1d1f8f40b80a0ed
Remi Puigsech <remi.puigsech@u-bordeaux.fr>
2025-01-08 12:05:55 +0100

    import

--- KickScore/src/Controller/MeetController.php
+++ KickScore/src/Controller/MeetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Console\Style\SymfonyStyle;
 use App\Entity\Championship;
 use App\Entity\Team;
 use App\Entity\Versus;
@@ -29,8 +30,75 @@ class MeetController extends AbstractController
         ]);
     }
 
+    #[Route('/meet/import', name: 'app_meet_import', methods: ['POST'])]
+    #[Route('/meet/import', name: 'app_meet_import', methods: ['POST'])]
+    public function import(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$this->isGranted('ROLE_ORGANIZER')) {
+            throw $this->createAccessDeniedException('Only organizers can import meets.');
+        }
+    
+        $file = $request->files->get('file');
+        
+        if (!$file) {
+            $this->addFlash('error', 'No file uploaded.');
+            return $this->redirectToRoute('app_meet');
+        }
+    
+        $jsonData = file_get_contents($file->getPathname());
+        $championships = json_decode($jsonData, true);
+    
+        if (!$championships) {
+            $this->addFlash('error', 'Invalid JSON format.');
+            return $this->redirectToRoute('app_meet');
+        }
+    
+        foreach ($championships as $championshipData) {
+            $championship = new Championship();
+            $championship->setName($championshipData['name']);
+            $championship->setOrganizer($this->getUser());
+            
+            foreach ($championshipData['matches'] as $matchData) {
+                $match = new Versus();
+                $match->setChampionship($championship);
+                
+                // Get team entities from repository
+                $blueTeam = $entityManager->getRepository(Team::class)->find($matchData['BlueTeam']);
+                $greenTeam = $entityManager->getRepository(Team::class)->find($matchData['GreenTeam']);
+                
+                // Create teams if they don't exist
+                if (!$greenTeam) {
+                    $greenTeam = new Team();
+                    $greenTeam->setName($matchData['GreenTeam']);
+                    $entityManager->persist($greenTeam);
+                }
+                
+                if (!$blueTeam) {
+                    $blueTeam = new Team();
+                    $blueTeam->setName($matchData['BlueTeam']);
+                    $entityManager->persist($blueTeam);
+                }
+                $championship->addTeam($greenTeam);
+                $championship->addTeam($blueTeam);
+                $match->setBlueTeam($blueTeam);
+                $match->setGreenTeam($greenTeam);
+                $match->setBlueScore($matchData['BlueScore']);
+                $match->setGreenScore($matchData['GreenScore']);
+                $match->setDate(new \DateTime($matchData['Date']));
+    
+                $entityManager->persist($match);
+            }
+            
+            $entityManager->persist($championship);
+        }
+    
+        $entityManager->flush();
+        $this->addFlash('success', 'Championships and matches have been imported successfully.');
+        return $this->redirectToRoute('app_meet');
+    }
+
     #[Route('/meet/create', name: 'app_meet_create', methods: ['POST'])]
-    public function create(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger): Response
+    function create(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger): Response
     {
         if (!$this->isGranted('ROLE_ORGANIZER')) {
             throw $this->createAccessDeniedException('Only organizers can create meets.');
