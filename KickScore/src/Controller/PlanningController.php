2ba3455a94e86a6150ee3f65e832289189df3277
Sans Alexandre <alexandre.sans@etu.u-bordeaux.fr>
2025-01-09 17:55:32 +0100

    continuation of the plannig part

--- KickScore/src/Controller/PlanningController.php
+++ KickScore/src/Controller/PlanningController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Entity\Championship;
+use App\Entity\Team;
+use App\Entity\Versus;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PlanningController extends AbstractController
 {
     #[Route('/planning', name: 'app_planning')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $championships = $entityManager->getRepository(Championship::class)->findAll();
+        $teams = $entityManager->getRepository(Team::class)->findAll();
         return $this->render('planning/index.html.twig', [
             'controller_name' => 'PlanningController',
+            'championships' => $championships,
+            'teams' => $teams,
         ]);
     }
 }
