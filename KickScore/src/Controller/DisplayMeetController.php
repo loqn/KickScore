dc321af576afa44ee32060ea42fdeb6a74f5cae9
loqn <loancollomb@icloud.com>
2025-01-13 21:51:48 +0000

    better responsive for ui (menuing for control panel on md), some translations

--- KickScore/src/Controller/DisplayMeetController.php
+++ KickScore/src/Controller/DisplayMeetController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Championship;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Match;
+use App\Entity\Versus;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
